4968cbcc0c972de27ce5f1f66037601316bd3770
nazimboudeffa <boudeffa@gmail.com>
2023-06-18 01:58:02 +0200

    Added product list

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        BACKEND_URL: "http://192.168.0.15:9000",
+    },
+    images: {
+    remotePatterns: [
+            {
+            protocol: 'https',
+            hostname: 'medusa-public-images.s3.eu-west-1.amazonaws.com',
+            port: '',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
