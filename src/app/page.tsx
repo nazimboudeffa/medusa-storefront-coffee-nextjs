7bd16b1070fa01a925ca4d7e7289f6788c96c7de
nazimboudeffa <boudeffa@gmail.com>
2023-06-18 14:34:47 +0200

    Added basic componenets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,11 @@
 "use client"
-import { useRef, useState, useEffect } from 'react'
-import Header from '@/components/Header'
-import ProductCard from '@/components/ProductCard'
-import { medusa } from '@/utils/medusa-client'
+import Hero from '@/components/Hero'
 
 export default function Home() {
-  let coffeeRef = useRef<HTMLParagraphElement>()
-
-  const scrollHandler = (e) => {
-    e.preventDefault()
-    // @ts-ignore
-    coffeeRef.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    })
-  }
-
-  const [products, setProducts] = useState([])
-  useEffect(() => {
-    const getProducts = async () => {
-        const results = await medusa.products.list();
-        console.log(results)
-        setProducts(results.products)
-    }
-    getProducts()
-  }, []);
-
   return (
-    <>
-      <Header scrollHandler={scrollHandler} />
-      <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <p
-              className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl"
-              ref={coffeeRef}
-            >
-              Crafted by us, for you
-            </p>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-              <ProductCard product={product} key={product.id} />
-            ))}
-        </div>
-      </div>
-    </>
+    <main>
+      <Hero />
+    </main>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,146 @@
-import Image from 'next/image'
-import img from '../../public/hero.jpg'
+import Link from "next/link";
 
-export default function Header({ scrollHandler }) {
-  return (
-    <header className="relative">
-      <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
-      <div className="mx-auto">
-        <div className="relative shadow-xl sm:overflow-hidden">
-          <div className="absolute inset-0">
-            <Image
-              priority
-              fill
-              className="h-full w-full object-cover"
-              src={img}
-              placeholder="blur"
-              alt="Coffee grinder"
-            />
-            <div className="absolute inset-0 bg-orange-100 mix-blend-multiply" />
-          </div>
-          <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-            <p className="relative left-0 right-0 mx-auto mt-5 max-w-xl text-center text-xl  font-semibold uppercase tracking-wide text-orange-600">
-              The Coffee House
-            </p>
-            <h1 className="mt-1 text-center font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-7xl">
-              <span className="block text-white">Life is better with</span>
-              <span className="block text-orange-500">coffee</span>
-            </h1>
+export default function Header () {
+    return (
+        <header className="border-b border-gray-100">
+        <div
+            className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8"
+        >
+            <div className="flex items-center gap-4">
+            <button type="button" className="p-2 lg:hidden">
+                <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                />
+                </svg>
+            </button>
 
-            <div className="mx-auto mt-10 max-w-xs sm:flex sm:max-w-none sm:justify-center">
-              <button
-                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-orange-600 shadow-sm hover:bg-orange-100 sm:px-8"
-                onClick={scrollHandler}
-              >
-                Shop swags
-              </button>
+            <Link href="/" className="flex">
+                <span className="sr-only">Logo</span>
+                <span className="inline-block h-10 w-32 rounded-lg bg-orange-500 text-white pl-10 pt-2">ACME</span>
+            </Link>
             </div>
-          </div>
+
+            <div className="flex flex-1 items-center justify-end gap-8">
+            <nav
+                aria-label="Global"
+                className="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500"
+            >
+                <a
+                href="/about"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-500"
+                >
+                About
+                </a>
+
+                <a
+                href="/news"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-500"
+                >
+                News
+                </a>
+
+                <a
+                href="/products"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-500"
+                >
+                Products
+                </a>
+
+                <a
+                href="/contact"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-500"
+                >
+                Contact
+                </a>
+            </nav>
+
+            <div className="flex items-center">
+                <div className="flex items-center border-x border-gray-100">
+                <span className="border-e border-e-gray-100">
+                    <a
+                    href="/cart"
+                    className="grid h-16 w-16 place-content-center border-b-4 border-transparent hover:border-orange-500"
+                    >
+                    <svg
+                        className="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                        />
+                    </svg>
+
+                    <span className="sr-only">Cart</span>
+                    </a>
+                </span>
+
+                <span className="border-e border-e-gray-100">
+                    <a
+                    href="/account"
+                    className="grid h-16 w-16 place-content-center border-b-4 border-transparent hover:border-orange-500"
+                    >
+                    <svg
+                        className="h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                        />
+                    </svg>
+
+                    <span className="sr-only"> Account </span>
+                    </a>
+                </span>
+
+                <span className="hidden sm:block">
+                    <a
+                    href="/search"
+                    className="grid h-16 w-16 place-content-center border-b-4 border-transparent hover:border-orange-500"
+                    >
+                    <svg
+                        className="h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                    </svg>
+
+                    <span className="sr-only"> Search </span>
+                    </a>
+                </span>
+                </div>
+            </div>
+            </div>
         </div>
-      </div>
-    </header>
-  )
+        </header>
+    )
 }
