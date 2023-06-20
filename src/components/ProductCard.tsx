47cf2ee4f1cff85e32c2b408a2c55ce41540bf8a
nazimboudeffa <boudeffa@gmail.com>
2023-06-20 23:12:21 +0200

    Added Product page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,7 +10,7 @@ export default function ProductCard({ product } : { product: any }) {
   const [isLoading, setLoading] = useState(true)
 
   return (
-    <Link href="#" className="group">
+    <Link href={`products/${product.handle}`} className="group">
       <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt="product image"
